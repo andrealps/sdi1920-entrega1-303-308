8784fd3f8b8264f2892528d16f76effabfa45864
Andrea López Suárez <uo264703@uniovi.es>
2020-03-08 13:18:29 +0100

    Validador de registro y barra de navegación internacionalizada

--- sdi1920-entrega1-303-308/src/main/java/com/uniovi/controllers/UsersController.java
+++ sdi1920-entrega1-303-308/src/main/java/com/uniovi/controllers/UsersController.java
@@ -11,20 +11,21 @@ import org.springframework.web.bind.annotation.*;
 import com.uniovi.entities.*;
 import com.uniovi.services.SecurityService;
 import com.uniovi.services.UsersService;
+import com.uniovi.validators.SignUpFormValidator;
 
 @Controller
 public class UsersController {
 //	@Autowired
 //	private RolesService rolesService;
-	
+
 	@Autowired
 	private UsersService usersService;
 
 	@Autowired
 	private SecurityService securityService;
 
-//	@Autowired
-//	private SignUpFormValidator signUpFormValidator;
+	@Autowired
+	private SignUpFormValidator signUpFormValidator;
 
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public String signup(Model model) {
@@ -34,6 +35,10 @@ public class UsersController {
 
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
 	public String signup(@Validated User user, BindingResult result) {
+		signUpFormValidator.validate(user, result);
+		if (result.hasErrors()) {
+			return "signup";
+		}
 		usersService.addUser(user);
 		securityService.autoLogin(user.getEmail(), user.getPasswordConfirm());
 		return "redirect:home";
